0d3b64383d260de69475a485d369ad5cc9439622
ci-robot <ci-robot@k8s.io>
2025-09-19 16:53:47 +0000

    Update to ACK runtime `v0.52.0`, code-generator `v0.52.0`

--- pkg/resource/workspace/resource.go
+++ pkg/resource/workspace/resource.go
@@ -97,11 +97,11 @@ func (r *resource) SetIdentifiers(identifier *ackv1alpha1.AWSIdentifiers) error 
 
 // PopulateResourceFromAnnotation populates the fields passed from adoption annotation
 func (r *resource) PopulateResourceFromAnnotation(fields map[string]string) error {
-	tmp, ok := fields["workspaceID"]
+	f0, ok := fields["workspaceID"]
 	if !ok {
 		return ackerrors.NewTerminalError(fmt.Errorf("required field missing: workspaceID"))
 	}
-	r.ko.Status.WorkspaceID = &tmp
+	r.ko.Status.WorkspaceID = &f0
 
 	return nil
 }
