21a2a2e8a35bbaca859fe20fbf03eab225ae49d3
ci-robot <ci-robot@k8s.io>
2026-06-20 18:46:31 +0000

    Update to ACK runtime `v0.59.2-0.20260617181621-38bd4c967a01`, code-generator `v0.60.0`

--- pkg/resource/logging_configuration/references.go
+++ pkg/resource/logging_configuration/references.go
@@ -25,6 +25,7 @@ import (
 
 	ackv1alpha1 "github.com/aws-controllers-k8s/runtime/apis/core/v1alpha1"
 	ackerr "github.com/aws-controllers-k8s/runtime/pkg/errors"
+	ackrt "github.com/aws-controllers-k8s/runtime/pkg/runtime"
 	acktypes "github.com/aws-controllers-k8s/runtime/pkg/types"
 
 	svcapitypes "github.com/aws-controllers-k8s/prometheusservice-controller/apis/v1alpha1"
@@ -100,9 +101,17 @@ func (rm *resourceManager) resolveReferenceForWorkspaceID(
 		if arr.Name == nil || *arr.Name == "" {
 			return hasReferences, fmt.Errorf("provided resource reference is nil or empty: WorkspaceRef")
 		}
-		namespace := ko.ObjectMeta.GetNamespace()
-		if arr.Namespace != nil && *arr.Namespace != "" {
-			namespace = *arr.Namespace
+		namespace, err := ackrt.ResolveCrossNamespaceReference(
+			ctx,
+			rm.cfg.EnableCrossNamespace,
+			&ko.Status.Conditions,
+			ackrt.CrossNamespaceRefKindResource,
+			ko.ObjectMeta.GetNamespace(),
+			arr.Namespace,
+			*arr.Name,
+		)
+		if err != nil {
+			return hasReferences, err
 		}
 		obj := &svcapitypes.Workspace{}
 		if err := getReferencedResourceState_Workspace(ctx, apiReader, obj, *arr.Name, namespace); err != nil {
