32a4ee57e4a8659cfac4ae4897252a07b01f3640
ci-robot <ci-robot@k8s.io>
2026-08-28 19:03:49 +0000

    Update to ACK runtime `v0.63.0`, code-generator `v0.63.0`

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/aws-controllers-k8s/prometheusservice-controller
 go 1.25.0
 
 require (
-	github.com/aws-controllers-k8s/runtime v0.62.0
+	github.com/aws-controllers-k8s/runtime v0.63.0
 	github.com/aws/aws-sdk-go v1.49.0
 	github.com/aws/aws-sdk-go-v2 v1.34.0
 	github.com/aws/aws-sdk-go-v2/service/amp v1.30.10
--- pkg/version/version.go
+++ pkg/version/version.go
@@ -27,6 +27,6 @@ var (
 // that generated this controller's code. They are baked in at code-generation
 // time and are immutable for the life of the generated code.
 const (
-	ACKGenerateVersion   = "v0.62.1"
-	ACKGenerateBuildDate = "2026-08-13T01:06:28Z"
+	ACKGenerateVersion   = "v0.63.0"
+	ACKGenerateBuildDate = "2026-08-28T19:03:43Z"
 )
